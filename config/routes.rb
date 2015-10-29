eb3f28c8ce9a6fa634c532e13010fc5bc12b62ad
James Chen <james.chen208@gmail.com>
2015-10-29 14:01:30 -0400

    remove devise sign up function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+ devise_for :users
  resources :posts
  resources :contacts, only: [:new, :create]
  resources :projects
  get 'welcome/index'
  root 'welcome#index'
+
+ get '*path' => redirect('/')
 end
